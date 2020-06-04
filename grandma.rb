f7288db24324b72e806763423ca758974f506635
langan.kg Langan <langan.kg@gmail.com>
2020-06-04 14:35:50 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -7,7 +7,7 @@ def speak_to_grandma(string)
   elsif string == string.upcase && != "I LOVE YOU GRANDMA!" 
       return "NO, NOT SINCE 1938!"
 
-  else  string == "I LOVE YOU GRANDMA!" 
+  else  string == "I LOVE YOU GRANDMA!"
       return "I LOVE YOU TOO PUMPKIN!"
 
   end
