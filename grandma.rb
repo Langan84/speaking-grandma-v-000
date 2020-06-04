ab500ba57584e0531cdd9263560ae2514a343988
langan.kg Langan <langan.kg@gmail.com>
2020-06-04 14:21:06 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,14 +1,15 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(string)
 
-  if  string == string.downcase
-      puts "HUH?! SPEAK UP, SONNY!"
+  if string == "I LOVE YOU GRANDMA!"
+      puts "I LOVE YOU TOO PUMPKIN!"
 
   elsif string == string.upcase
       puts "NO, NOT SINCE 1938!"
 
-  else string == "I LOVE YOU GRANDMA!"
-      puts "I LOVE YOU TOO PUMPKIN!"
+  else
+      puts "HUH?! SPEAK UP, SONNY!"
+
   end
 end
 # Whatever you say to grandma, she should respond with
