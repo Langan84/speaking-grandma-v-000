40255fa9a3dfbea4b2f802d9af850781605db229
langan.kg Langan <langan.kg@gmail.com>
2020-06-04 14:24:12 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -5,7 +5,7 @@ phrase = gets.chomp
   if phrase == "I LOVE YOU GRANDMA!"
       puts "I LOVE YOU TOO PUMPKIN!"
 
-  elsif string == string.upcase
+  elsif phrase == string.upcase
       puts "NO, NOT SINCE 1938!"
 
   else
