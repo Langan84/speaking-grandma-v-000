dfebada8e3ee8d4700c26417c1b3d16668a596ce
langan.kg Langan <langan.kg@gmail.com>
2020-06-04 14:31:09 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,14 +1,14 @@
 # Write a speak_to_grandma method.
-def speak_to_grandma(phrase)
+def speak_to_grandma(string)
 
 
-  if phrase != string.upcase
+  if string != string.upcase
       return "HUH?! SPEAK UP, SONNY!"
 
-  elsif phrase == string.upcase
+  elsif string == string.upcase
       return "NO, NOT SINCE 1938!"
 
-  else phrase == "I LOVE YOU GRANDMA!"
+  else string == "I LOVE YOU GRANDMA!"
       return "I LOVE YOU TOO PUMPKIN!"
 
   end
